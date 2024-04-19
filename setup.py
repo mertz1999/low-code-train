9aed1e79d77f1e0200e5ff863af29289b0691597
Reza Tanakizadeh <reza.tz780210@gmail.com>
2024-04-19 21:33:49 +0330

    (TEST)

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mypackage',
+    version='0.1',
+    packages=find_packages(),
+    description='A simple example Python package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Reza Tanakizadeh',
+    author_email='your.email@example.com',
+    url='https://github.com/mertz1999/low-code-train',
+    license='MIT',
+    install_requires=[
+        # List all packages that your package depends on:
+        'numpy',
+        'pandas',
+    ],
+    python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
